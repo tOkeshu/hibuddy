9aa49f64ce4642b16d74f4cafd084654a78a83de
Romain Gauthier <romain.gauthier@monkeypatch.me>
2012-09-19 17:57:05 +0200

    Initial revision of "HiBuddy".
    HiBuddy is a web application to make simple videoconferences.

---
+++ README.md
@@ -0,0 +1,18 @@
+# HiBuddy, say hi to your friends !
+
+HiBuddy provides you a simple way to make bidirectional
+videoconferences with your friends.
+
+## Goals
+
+  - Provides a small web application.
+  - Easy to install, easy to launch, easy to use.
+  - Doesn't keep any user data.
+  - Free as in freedom.
+
+## License
+
+Fipes is released under the terms of the
+[GNU Affero General Public License v3](http://www.gnu.org/licenses/agpl-3.0.html)
+or later.
+
---
+++ hibuddy/src/hibuddy.erl
@@ -0,0 +1,44 @@
+-module(hibuddy).
+-behaviour(application).
+
+-export([start/0, start/2, stop/1]).
+-define(PUBLIC, [<<"public">>]).
+
+
+start() ->
+    application:start(hibuddy).
+
+% GET    /             => landing page
+% POST   /rooms        => new room
+% GET    /room/{room}  => get into the room
+% DELETE /room/{room}  => destroy the room
+
+% GET    /rooms        => not allowed
+% POST   /rooms/{room} => not allowed
+
+start(_Type, _Args) ->
+    Routes =
+        [{'_', [{[<<"rooms">>, room],   hibuddy_rooms, []},
+                {[<<"static">>, '...'], cowboy_http_static,
+                 [{directory, ?PUBLIC},
+                  {mimetypes, {fun mimetypes:path_to_mimes/2, default}}]},
+                {['...'],                                cowboy_http_static,
+                 [{directory, ?PUBLIC},
+                  {mimetypes, {fun mimetypes:path_to_mimes/2, default}}]}
+               ]}],
+
+    cowboy:start_listener(http,100,
+                          cowboy_tcp_transport, [{port, 6424}],
+                          cowboy_http_protocol, [{dispatch, Routes}]
+                         ),
+    cowboy:start_listener(https, 100,
+                          cowboy_ssl_transport, [{port,     6425},
+                                                 {certfile, "priv/ssl/cert.pem"},
+                                                 {keyfile,  "priv/ssl/key.pem"},
+                                                 {password, "cowboy"}],
+                          cowboy_http_protocol, [{dispatch, Routes}]),
+    hibuddy_sup:start_link().
+
+
+stop(_State) ->
+    ok.
