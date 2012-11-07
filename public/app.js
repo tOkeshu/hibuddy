ee2f8dfe9f401f87d69a6ed14d290e29db618097
Romain Gauthier <romain.gauthier@monkeypatch.me>
2012-11-07 21:54:08 +0100

    App: get the local audio and video streams

--- public/app.js
+++ public/app.js
@@ -1,63 +1,30 @@
 $(document).ready(function() {
+    var localVideo = $('#local-video').get(0);
+    var localAudio = $('#local-audio').get(0);
+    var remoteVideo = $('#remote-video').get(0);
+    var peerConnection = new mozRTCPeerConnection();
 
-    var video1 = $('#video1')[0];
-    var interv;
+    // Get local video
+    navigator.mozGetUserMedia({video: true}, function(stream) {
+        localVideo.mozSrcObject = stream;
+        localVideo.play();
+        peerConnection.addStream(stream);
 
-    $('#start-video').click(function() {
-        window.navigator.mozGetUserMedia({video: true}, function(stream) {
-            video1.mozSrcObject = stream;
-            video1.play();
-            open(function() {
-                interv = setInterval(capture, 200);
+        // Get local audio
+        navigator.mozGetUserMedia({audio: true},function(stream) {
+            localAudio.mozSrcObject = stream;
+            localAudio.play();
+            peerConnection.addStream(stream);
+
+            // Create offer
+            peerConnection.createOffer(function(offer) {
+                peerConnection.setLocalDescription(offer, function() {
+                    // Send offer
+                });
             });
-        }, function(err) {
-            video1.pause();
-            video1.src = '';
-            clearInterval(interv);
-        });
-    });
 
-    $('#stop-video').click(function() {
-        video1.pause();
-        video1.src = '';
-        clearInterval(interv);
-    });
-
-    var open = function(callback) {
-        var uri = "ws://" + location.host + ':6424/rooms/1234';
-        window.ws = new WebSocket(uri);
-
-        window.ws.onopen = function() {
-            // websocket is connected
-            console.log("websocket connected!");
-            callback();
-        };
-
-        window.ws.onclose = function() {
-            // websocket was closed
-            console.log("websocket was closed");
-            video1.pause();
-            video1.src = '';
-            clearInterval(interv);
-        };
-    };
-
-    var capture = function(){
-        var width  = video1.clientWidth;
-        var height = video1.clientHeight;
-
-        var buffer = document.createElement('canvas');
-        var bufferContext = buffer.getContext('2d');
-        var canvas = $('#feedback')[0];
-        var context = canvas.getContext('2d');
-
-        buffer.width = canvas.width = width;
-        buffer.height = canvas.height = height;
-        bufferContext.drawImage(video1, 0, 0, width, height);
-
-        var dataUrl = buffer.toDataURL('image/jpeg');
-        window.ws.send(dataUrl);
-    };
+        }, function() {});
+    }, function() {});
 
 });
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use('/static', express.static(__dirname + '/public'));
 
 app.listen(6424);
 console.log('Listening on port 6424');
