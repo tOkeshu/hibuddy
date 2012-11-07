bba0af9a8e7a373493d7c32d35ed1476dce4f3c4
Romain Gauthier <romain.gauthier@monkeypatch.me>
2012-11-07 21:57:09 +0100

    Server: basic server-sent events.

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 var express = require('express');
 var app = express();
+var connections = [];
 
 app.use('/static', express.static(__dirname + '/public'));
+
+app.get("/signalling", function(req, res) {
+    console.log("/signalling connection opened");
+
+    res.writeHead(200, {
+        "Content-Type":  "text/event-stream",
+        "Cache-Control": "no-cache",
+        "Connection":    "keep-alive"
+    });
+
+    req.on("close", function() {
+        var i = connections.indexOf(res);
+        connections.splice(i, 1);
+    });
+});
+
 
 app.listen(6424);
 console.log('Listening on port 6424');
