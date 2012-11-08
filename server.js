4c09ca394562aa716e7e2fe9c46b8a9841288da2
Romain Gauthier <romain.gauthier@monkeypatch.me>
2012-11-08 19:40:38 +0100

    Server: add a pubsub tiny API.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var connections = [];
 var counter = 0;
 
+app.use(express.bodyParser());
 app.use('/static', express.static(__dirname + '/public'));
 
 app.get("/signalling", function(req, res) {
@@ -24,6 +25,18 @@ app.get("/signalling", function(req, res) {
     res.write("data: " + event + "\n\n");
 });
 
+app.post("/signalling", function(req, res) {
+    var type = req.body.type;
+    var event = JSON.stringify(req.body);
+    console.log(req.body);
+
+    connections.map(function(c) {
+        c.write("event: " + type + "\n");
+        c.write("data: " + event + "\n\n");
+    });
+
+    res.send(200);
+});
 
 app.listen(6424);
 console.log('Listening on port 6424');
