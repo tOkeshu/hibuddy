56524a0a43f9561bb4049991979c47fd62ae8c05
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-12-12 15:24:20 +0100

    Room: rewrite the application in an OO manner

--- public/app.js
+++ public/app.js
@@ -1,193 +1,20 @@
-/* globals EventSource, RSVP, _ , ScratchArea,
-   mozRTCSessionDescription, mozRTCPeerConnection
- */
+(function() {
+  var room = window.location.pathname.split('/')[2];
+  var hibuddy = new HiBuddyApp(room);
 
-function notify(message, type) {
-  var alert = document.querySelector(".alert");
-  alert.innerHTML = message;
-  if (type)
-    alert.classList.add("alert-" + type);
-  alert.classList.remove("hidden");
-}
+  var localVideo  = document.getElementById('local-video');
+  var remoteVideo = document.getElementById('remote-video');
+  navigator.mozGetUserMedia({video: true, audio: true}, function(localStream) {
+    localVideo.mozSrcObject = localStream;
+    localVideo.play();
 
-document.addEventListener('DOMContentLoaded', function() {
-  if (window.mozRTCPeerConnection === undefined ||
-      navigator.mozGetUserMedia === undefined)
-    notify("This application only works with Firefox 25 or later. " +
-           "Please consider <a href=\"https://github.com/tOkeshu/hibuddy\">" +
-           "contributing to hibbudy</a> if you want Chrome support :)");
-
-  var config = {
-    iceServers: [{
-      // please contact me if you plan to use this server
-      url: 'turn:webrtc.monkeypatch.me:6424?transport=udp',
-      credential: 'hibuddy',
-      username: 'hibuddy'
-    }]
-  };
-  var peerConnection = new mozRTCPeerConnection(config), dataChannel;
-  var room           = window.location.pathname.split('/')[2];
-  var source         = new EventSource("/rooms/" + room + "/signalling");
-  var me, scratcharea;
-
-  peerConnection.onaddstream = function(obj) {
-    var remoteVideo = document.getElementById('remote-video');
-    console.log(obj);
-
-    remoteVideo.mozSrcObject = obj.stream;
-    remoteVideo.play();
-  };
-
-  peerConnection.oniceconnectionstatechange = function() {
-    // TODO: display an error if the ice connection failed
-    console.log("ice: " + peerConnection.iceConnectionState);
-    if (peerConnection.iceConnectionState === "failed")
-      notify("Something went wrong: the connection failed", "error");
-  };
-
-  peerConnection.onicecandidate = function(event) {
-    if (event.candidate) {
-      var candidate = {
-        candidate: event.candidate.candidate,
-        sdpMid: event.candidate.sdpMid,
-        sdpMLineIndex: event.candidate.sdpMLineIndex
-      };
-      post({type: 'iceCandidate', from: me, candidate: candidate});
-    }
-  };
-
-  var post = function(data) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/rooms/' + room + '/signalling', true);
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xhr.send(JSON.stringify(data));
-  };
-
-  var getVideoAudio = function(callback) {
-    var localVideo = document.getElementById('local-video');
-
-    navigator.mozGetUserMedia({video: true, audio: true}, function(stream) {
-      localVideo.mozSrcObject = stream;
-      localVideo.play();
-      peerConnection.addStream(stream);
-
-      callback();
-    }, function(err) {
-      callback(err);
+    hibuddy.start(localStream, function(remoteStream) {
+      remoteVideo.mozSrcObject = remoteStream;
+      remoteVideo.play();
     });
-  };
 
-  var sendOffer, waitFriend;
-  sendOffer = waitFriend = _.after(2, function() {
-    // Create offer
-    peerConnection.createOffer(function(offer) {
-      peerConnection.setLocalDescription(offer, function() {
-        // Send offer
-        post({type: 'offer', from: me, offer: offer});
-      });
-    }, function() {});
+  }, function(err) {
+    console.error("getUserMedia Failed");
   });
 
-  var sendAnswer, waitOffer;
-  sendAnswer = waitOffer = _.after(2, function() {
-    // Create answer
-    peerConnection.createAnswer(function(answer) {
-      peerConnection.setLocalDescription(answer, function() {
-        // Send answer
-        post({type: 'answer', from: me, answer: answer});
-      });
-    }, function() {});
-  });
-
-  // XXX: handle errors
-  getVideoAudio(function(err) {
-    if (err)
-      notify("Something went wrong: " + err, "error");
-
-    waitFriend();
-    waitOffer();
-  });
-
-  source.addEventListener("uid", function(event) {
-    event = JSON.parse(event.data);
-    me    = event.uid;
-    console.log('UID: ' + me);
-  });
-
-  source.addEventListener("newfriend", function(event) {
-    event = JSON.parse(event.data);
-    sendOffer();
-  });
-
-  source.addEventListener("offer", function(event) {
-    event = JSON.parse(event.data);
-
-    if (event.from === me)
-      return;
-
-    var offer = new mozRTCSessionDescription(event.offer);
-    peerConnection.setRemoteDescription(offer, function() {
-      sendAnswer();
-    });
-  });
-
-  source.addEventListener('answer', function(event) {
-    event = JSON.parse(event.data);
-    console.log(event.answer);
-
-    if (event.from === me)
-      return;
-
-    var answer = new mozRTCSessionDescription(event.answer);
-    peerConnection.setRemoteDescription(answer, function() {
-      console.log('done');
-    });
-  });
-
-  source.addEventListener("iceCandidate", function(event) {
-    event = JSON.parse(event.data);
-
-    if (event.from === me)
-      return;
-
-    peerConnection.addIceCandidate(new mozRTCIceCandidate(event.candidate));
-  });
-
-
-  // Fullscreen
-  function fullscren() { this.parentNode.mozRequestFullScreen(); }
-  document.getElementById('local-video').addEventListener('click', fullscren);
-  document.getElementById('remote-video').addEventListener('click', fullscren);
-
-  dataChannel = peerConnection.createDataChannel('dc', {
-    id: 0,
-    negotiated: true,
-    // Stream and preset parameters enable backwards compatibility
-    // from Firefox 24 until bug 892441 is fixed.
-    stream: 0,
-    preset: true
-  });
-
-  scratcharea = new ScratchArea({
-    node: document.querySelector("textarea"),
-    transport: dataChannel
-  });
-
-  dataChannel.onopen = function() {
-    scratcharea.node.removeAttribute("disabled");
-    scratcharea.monitor();
-  };
-
-  dataChannel.onerror = function(e) {
-    scratcharea.node.removeAttribute("disabled");
-    scratcharea.stop();
-    console.error("DataChannel Error: " + e);
-  };
-
-  dataChannel.onclose = function() {
-    scratcharea.node.setAttribute("disabled", "disabled");
-    scratcharea.stop();
-  };
-
-});
-
+}());
---
+++ public/hibuddy_app.js
@@ -0,0 +1,135 @@
+/* globals */
+
+
+function HiBuddyApp(room) {
+  this.room = room;
+  this.me = undefined;
+}
+
+HiBuddyApp.prototype = {
+  start: function(stream, callback) {
+    this.stream = stream;
+    this.onRemoteStream = callback;
+
+    this.source = new EventSource("/rooms/" + this.room + "/signalling");
+    this.source.on = this.source.addEventListener.bind(this.source);
+    this.source.on("uid",          this._onUID.bind(this));
+    this.source.on("newfriend",    this._onNewBuddy.bind(this));
+    this.source.on("offer",        this._onOffer.bind(this));
+    this.source.on("answer",       this._onAnswer.bind(this));
+    this.source.on("icecandidate", this._onIceCandidate.bind(this));
+  },
+
+  _onUID: function(event) {
+    var message = JSON.parse(event.data);
+    this.me = message.uid;
+    console.log('UID: ' + this.me);
+  },
+
+  _onNewBuddy: function() {
+    var peerConnection = new mozRTCPeerConnection(this.config);
+    this.peerConnection = this._setupPeerConnection(peerConnection);
+    this._sendOffer();
+  },
+
+  _onOffer: function(event) {
+    var message = JSON.parse(event.data);
+    var peerConnection = new mozRTCPeerConnection(this.config);
+
+    if (message.from === this.me)
+      return;
+
+    this.peerConnection = this._setupPeerConnection(peerConnection);
+
+    var offer = new mozRTCSessionDescription(message.offer);
+    this.peerConnection.setRemoteDescription(offer, function() {
+      this._sendAnswer();
+    }.bind(this));
+  },
+
+  _onAnswer: function(event) {
+    var message = JSON.parse(event.data);
+    console.log(message.answer);
+
+    if (message.from === this.me)
+      return;
+
+    var answer = new mozRTCSessionDescription(message.answer);
+    this.peerConnection.setRemoteDescription(answer, function() {
+      console.log("done");
+    }.bind(this));
+  },
+
+  _onIceCandidate: function(event) {
+    var message = JSON.parse(event.data);
+
+    if (message.from === this.me)
+      return;
+
+    var candidate = new mozRTCIceCandidate(message.candidate);
+    this.peerConnection.addIceCandidate(candidate);
+  },
+
+  _onIceStateChange: function(event) {
+    // XXX: display an error if the ice connection failed
+    console.log("ice: " + this.peerConnection.iceConnectionState);
+    if (this.peerConnection.iceConnectionState === "failed") {
+      console.error("Something went wrong: the connection failed");
+    }
+
+    if (this.peerConnection.iceConnectionState === "connected")
+      console.log("connected");
+  },
+
+  _onNewIceCandidate: function(event) {
+    if (event.candidate) {
+      var candidate = {
+        candidate: event.candidate.candidate,
+        sdpMid: event.candidate.sdpMid,
+        sdpMLineIndex: event.candidate.sdpMLineIndex
+      };
+      this._post({type: 'iceCandidate', from: this.me, candidate: candidate});
+    }
+  },
+
+  _onAddStream: function(event) {
+    this.onRemoteStream(event.stream);
+  },
+
+  _setupPeerConnection: function(pc) {
+    pc.onaddstream = this._onAddStream.bind(this);
+    pc.oniceconnectionstatechange = this._onIceStateChange.bind(this);
+    pc.onicecandidate = this._onNewIceCandidate.bind(this);
+    pc.addStream(this.stream);
+    return pc;
+  },
+
+  _sendOffer: function() {
+    // Create offer
+    this.peerConnection.createOffer(function(offer) {
+      this.peerConnection.setLocalDescription(offer, function() {
+        // Send offer
+        this._post({type: 'offer', from: this.me, offer: offer});
+      }.bind(this));
+    }.bind(this), function() {});
+  },
+
+  _sendAnswer: function() {
+    // Create answer
+    this.peerConnection.createAnswer(function(answer) {
+      this.peerConnection.setLocalDescription(answer, function() {
+        // Send answer
+        this._post({type: 'answer', from: this.me, answer: answer});
+      }.bind(this));
+    }.bind(this), function() {});
+  },
+
+  _post: function(data) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/rooms/' + this.room + '/signalling', true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xhr.send(JSON.stringify(data));
+  }
+
+};
+
