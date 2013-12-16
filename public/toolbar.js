a7d9aec46a69511aa1b192f22930634eb877e547
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-12-16 13:00:40 +0100

    Toolbar: implement the fullscreen button

--- public/toolbar.js
+++ public/toolbar.js
@@ -12,9 +12,13 @@ function HiBuddyToolbar(el, stream) {
 
   this.video = this.el.querySelector(".video");
   this.audio = this.el.querySelector(".audio");
+  this.fullscreen = this.el.querySelector(".fullscreen");
 
   this.video.addEventListener("click", this.toggleVideo.bind(this));
   this.audio.addEventListener("click", this.toggleAudio.bind(this));
+  this.fullscreen.addEventListener("click", this.toggleFullscreen.bind(this));
+  document.addEventListener("mozfullscreenchange",
+                            this.onFullscreenExit.bind(this));
 }
 
 HiBuddyToolbar.prototype = {
@@ -50,6 +54,30 @@ HiBuddyToolbar.prototype = {
     return function(track) {
       track.enabled = enabled;
     }
+  },
+
+  toggleFullscreen: function(event) {
+    event.preventDefault();
+    this.state.fullscreen = !this.state.fullscreen;
+
+    if (this.state.fullscreen) {
+      this.fullscreen.classList.remove("disabled");
+      this.fullscreen.classList.add("enabled");
+      document.querySelector("body").mozRequestFullScreen();
+    } else {
+      this.fullscreen.classList.remove("enabled");
+      this.fullscreen.classList.add("disabled");
+      document.mozCancelFullScreen();
+    }
+  },
+
+  onFullscreenExit: function() {
+    if (document.mozFullScreenElement)
+      return;
+
+    this.state.fullscreen = false;
+    this.fullscreen.classList.remove("enabled");
+    this.fullscreen.classList.add("disabled");
   }
 };
 
