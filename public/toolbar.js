77e10ff97d7cd6e19a36e6d2156b2ccf5dddce63
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-12-16 12:57:21 +0100

    Toolbar: implement the video mute button

--- public/toolbar.js
+++ public/toolbar.js
@@ -17,6 +17,22 @@ function HiBuddyToolbar(el, stream) {
 HiBuddyToolbar.prototype = {
   toggleVideo: function(event) {
     event.preventDefault();
+    this.state.video = !this.state.video;
+
+    if (this.state.video) {
+      this.video.classList.remove("disabled");
+      this.video.classList.add("enabled");
+    } else {
+      this.video.classList.remove("enabled");
+      this.video.classList.add("disabled");
+    }
+    this.stream.getVideoTracks().forEach(this._muter(this.state.video), this);
+  }
+
+  _muter: function(enabled) {
+    return function(track) {
+      track.enabled = enabled;
+    }
   }
 };
 
