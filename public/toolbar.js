5eacf2367c83607a1b068d2d7e415d7d0106b052
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-12-16 12:58:41 +0100

    Toolbar: implement the audio mute button

--- public/toolbar.js
+++ public/toolbar.js
@@ -11,7 +11,10 @@ function HiBuddyToolbar(el, stream) {
   };
 
   this.video = this.el.querySelector(".video");
+  this.audio = this.el.querySelector(".audio");
+
   this.video.addEventListener("click", this.toggleVideo.bind(this));
+  this.audio.addEventListener("click", this.toggleAudio.bind(this));
 }
 
 HiBuddyToolbar.prototype = {
@@ -27,7 +30,21 @@ HiBuddyToolbar.prototype = {
       this.video.classList.add("disabled");
     }
     this.stream.getVideoTracks().forEach(this._muter(this.state.video), this);
-  }
+  },
+
+  toggleAudio: function(event) {
+    event.preventDefault();
+    this.state.audio = !this.state.audio;
+
+    if (this.state.audio) {
+      this.audio.classList.remove("disabled");
+      this.audio.classList.add("enabled");
+    } else {
+      this.audio.classList.remove("enabled");
+      this.audio.classList.add("disabled");
+    }
+    this.stream.getAudioTracks().forEach(this._muter(this.state.audio), this);
+  },
 
   _muter: function(enabled) {
     return function(track) {
