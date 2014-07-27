a1b274317cebd43f57262b36f0a584d831bb9957
Romain Gauthier <romain.gauthier@monkeypatch.me>
2014-07-27 19:11:19 +0200

    Add a license header to the webrtc shim

--- public/webrtc-shim.js
+++ public/webrtc-shim.js
@@ -1,4 +1,16 @@
 (function(window, navigator) {
+  /* webrtc-shim.js A simple shim for WebRTC. - romain.gauthier@monkeypatch.me
+
+     To the extent possible under law, the author(s) have dedicated
+     all copyright and related and neighboring rights to this software
+     to the public domain worldwide. This software is distributed
+     without any warranty.
+
+     You should have received a copy of the CC0 Public Domain
+     Dedication along with this software. If not, see
+     <http://creativecommons.org/publicdomain/zero/1.0/>.
+  */
+
   navigator.getUserMedia = (navigator.getUserMedia       ||
                             navigator.mozGetUserMedia    ||
                             navigator.webkitGetUserMedia);
